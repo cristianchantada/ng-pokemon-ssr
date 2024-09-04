9d75dd99ee1b9dea7fb074edf10f4afb0e87e6c4
Cristian Varela Casas <cristianchantada@gmailcom>
2024-09-04 12:46:10 +0200

    Páginas creadas y configurado para utilizar SSR

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-contact',
@@ -7,6 +8,16 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class COntactPageComponent {
+export default class COntactPageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact Page');
+    this.meta.updateTag({name: 'description', content: 'Este es mi Contact page'});
+    this.meta.updateTag({name: 'og:title', content: 'Contact Page'});
+    this.meta.updateTag({name: 'keywords', content: 'Contact,PAge,Cristian,Angular'});
+  }
 
 }
--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, ChangeDetectionStrategy, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-pricing',
@@ -7,6 +9,23 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent {
+export default class PricingPageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+
+    // if(!isPlatformServer(this.platform)){
+    //   document.title = 'Pricing Page'
+    // }
+
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({name: 'description', content: 'Este es mi Pricing page'});
+    this.meta.updateTag({name: 'og:title', content: 'Princing Page'});
+    this.meta.updateTag({name: 'keywords', content: 'Pricing,PAge,Cristian,Angular'});
+  }
 
 }
+
